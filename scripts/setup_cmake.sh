6dcfff52ed29d19537ff91d4a78b77e7e94a3f24
Dylan Marcus <dylan@mach1studios.com>
2022-07-13 12:13:25 -0400

    cmake setup script

---
+++ scripts/setup_cmake.sh
@@ -0,0 +1,11 @@
+# cmake > 3.15
+cmake_version=3.15.7
+
+mkdir ~/temp
+cd ~/temp
+wget wget https://cmake.org/files/v3.15/cmake-3.15.7.tar.gz
+tar -xzvf cmake-3.15.7.tar.gz
+cd cmake-3.15.7/
+./bootstrap
+make
+sudo make install
