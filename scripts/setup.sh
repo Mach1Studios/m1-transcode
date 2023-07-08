ec1eda2beccee2bb1dd4b25b116d2e80c5e9e744
Dylan Marcus <dylan@mach1.tech>
2023-07-07 20:58:43 -0400

    fixed setup script to be more flexible for existing folders

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,21 +3,7 @@
 mkdir -p libs
 cd libs
 
-git clone --depth 1 git@github.com:Mach1Studios/m1-sdk.git
-git clone --depth 1 git@github.com:libsndfile/libsndfile.git
-git clone --depth 1 git@github.com:ebu/libadm.git
-git clone --depth 1 git@github.com:irt-open-source/libbw64.git
-
-# cd libadm
-# mkdir build && cd build
-# cmake ..
-# make
-# make install
-
-# cd ../../
-
-# cd libbw64
-# mkdir build && cd build
-# cmake ..
-# make
-# make install
+git -C m1-sdk pull || git clone --depth 1 git@github.com:Mach1Studios/m1-sdk.git m1-sdk
+git -C libsndfile pull || git clone --depth 1 git@github.com:libsndfile/libsndfile.git libsndfile
+git -C libadm pull || git clone --depth 1 git@github.com:ebu/libadm.git libadm
+git -C libbw64 pull || git clone --depth 1 git@github.com:irt-open-source/libbw64.git libbw64
